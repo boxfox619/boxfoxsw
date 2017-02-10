0d0570377b21b55be075a85e1959e0dd31f50f0c
Boxfox <rlatjdfo112@naver.com>
2017-02-10 12:58:16 +0000

    connect mongodb

--- homepage.js
+++ homepage.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient
+var Server = require('mongodb').Server;
+var MongoClient = require("mongodb").MongoClient;
+var dbs;
+
+MongoClient.connect("mongodb://localhost:5050/crunchbase", function(err, dbs){
+   console.log("connected to the DB");
+
+   var query = {"category_code" : "biotech"};
+   dbs.collection("companies").find(query).toArray(function(err, docs){
+       docs.forEach(function(doc){
+           console.log(doc.name + " is a " + doc.category_code  + "company")
+        });
+  })
+})
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', function(req, res){
-   res.render('index', { title: 'GCHOI' });
+   dbs.collection('users').findOne({},function(err,doc){
+       if(err) throw err;
+       res.send(doc);
+   });
 });
 
 app.get('/shakenote', function(req, res){
